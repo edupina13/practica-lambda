8d0be11197f7a2d240215e3d1bdcf984d967f3af
edupina13 <20213tn070@utez.edu.mx>
2024-05-25 22:44:32 -0600

    prueba cambios 4

--- hello_world/app.py
+++ hello_world/app.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
             "grado": "9",
             "grupo": "B",
             "edad": "40",
-            "estado": "Nuevo Leon",
+            "estado": "Morelos",
             # "location": ip.text.replace("\n", "")
         }),
     }
